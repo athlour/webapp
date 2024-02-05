9591a936900a22de62e61eaee11ff6dbcf138c59
Athlour Vinod <vinoihero@gmail.com>
2024-02-05 18:53:31 +0530

    Added Pipeline

--- test_app.py
+++ test_app.py
@@ -1,21 +1,35 @@
 import pytest
-import requests
 from flask import json
 from app import app
 
 
-
 @pytest.fixture
-def base_url():
-    return 'http://localhost:5000'  # Update the URL based on your Flask app's configuration
+def test_client():
+    # This fixture starts the Flask app for testing
+    with app.test_client() as client:
+        yield client
 
-def test_login_endpoint(base_url):
-    # Make a POST request to the /login endpoint
-    response = requests.post(f'{base_url}/login', headers={'Authorization': 'Basic dmlub2loZXJvQGdtYWlsLmNvbTpBd2FyZW5lc3MxOTg4ITE='})
+
+def test_login_endpoint(test_client):
+    # Use the test_client fixture to make requests to the Flask app
+    response = test_client.post('/login',
+                                headers={'Authorization': 'Basic dmlub2loZXJvQGdtYWlsLmNvbTpBd2FyZW5lc3MxOTg4ITE='})
 
     # Check if the response status code is 200 (OK)
     assert response.status_code == 200
 
+    # Extract cookies from the response headers
+
+    print (response.headers)
+
+    print(dir(response.headers))
+    values = [x for x in response.headers.values()]
+    cookie_token = values[2].split("=")
+    assert 'token' in cookie_token
+
+
+
+
+
     # Check if the 'token' cookie is set in the response
-    assert 'token' in response.cookies
 
